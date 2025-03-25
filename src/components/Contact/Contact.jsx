b85139f6722c39f9088a5afc0953032531f358ae
Vishakha Thakur <vishakhat895@gmail.com>
2025-03-25 10:51:07 +0530

    ok

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,34 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <div className={styles.linkedin}>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:vishakharawat952@gmail.com">vishakharawat952@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/vishakharawat/" target="_blank">linkedin.com/vishakharawat</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/vishakha952" target="_blank">github.com/vishakha952</a>
+        </li>
+      </ul></div>
+    </footer>
+  );
+};
