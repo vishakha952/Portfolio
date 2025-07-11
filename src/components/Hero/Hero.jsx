b33945414b6c9af5e6276a8f2de732e9c84ed864
Vishakha  Rawat <vishakhat895@gmail.com>
2025-07-11 19:15:12 +0530

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
   // Function to handle CV download
   const handleDownloadCV = () => {
     // Replace 'cv.pdf' with the path to your actual CV file
-    const downloadUrl = getImageUrl("hero/Vishakha_Resume.pdf");
+    const downloadUrl = getImageUrl("hero/new_resume.pdf");
     window.open(downloadUrl, "_blank");
   };
   return (
